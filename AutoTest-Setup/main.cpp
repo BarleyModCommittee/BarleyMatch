ee9d188d3ad91dcb74c8ac6642889381eb66db8f
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2026-05-27 16:54:05 +0800

    fix(AutoTest): setup problem

--- AutoTest-Setup/main.cpp
+++ AutoTest-Setup/main.cpp
@@ -7,6 +7,21 @@ using nlohmann::json;
 
 wchar_t dir[1000];
 
+uint8_t code[] =
+{
+	0x68, 0, 0, 0, 0,
+	0xFF, 0x15, 0xA4, 0x20, 0x65, 0,
+
+	0x85, 0xC0,
+	0x75, 9,
+
+	0x6A, 0xFF,
+	0xB8, 0x92, 0xDD, 0x61, 0,
+	0xFF, 0xD0
+};
+
+char dll_pos[] = "../MatchCore.dll";
+
 int main(int argc, char* argv[])
 {
 	std::ifstream config{"file.json"};
@@ -31,7 +46,13 @@ int main(int argc, char* argv[])
 		if (!draw_board)
 			DisableBoardDraw();
 
-		PVZ::Memory::InjectDll("../AutoTest-Core.dll");
+		auto pos = (int*)(code + 1);
+		*pos = PVZ::Memory::Variable;
+
+		PVZ::Memory::WriteArray(PVZ::Memory::Variable, dll_pos, sizeof(dll_pos));
+
+		Injector inj(0x5513B5, 6, code, sizeof(code));
+
 		PVZ::QuitPVZ();
 	}
 	std::system("pause");
