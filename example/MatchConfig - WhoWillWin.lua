90de7664a60d82358b35164fdfd39b10704ab2bf
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2026-08-27 15:44:16 +0800

    fix: IZ example

--- example/MatchConfig - WhoWillWin.lua
+++ example/MatchConfig - WhoWillWin.lua
@@ -1,6 +1,9 @@
 -- MatchConfig - WhoWillWin.lua
 -- 测试环境：谁会赢？测试
 
+local zombie_win_round = 0
+local total = 10000
+
 -- 基础配置
 function isPoolEnabled()
 	return false  -- 5行模式（无泳池）
@@ -19,7 +22,7 @@ function shouldDrawBoard()
 end
 
 function isIZMode()
-	return false  -- 是否为 IZ 模式
+	return true  -- 是否为 IZ 模式
 end
 
 -- 输出文件
@@ -28,7 +31,7 @@ local result_file = nil
 -- 生成文件名（带时间戳）
 local function generate_filename()
 	local time = os.date("*t")
-	return string.format("result_%04d%02d%02d_%02d%02d%02d.csv",
+	return string.format("result_%04d%02d%02d_%02d%02d%02d.jsonl",
 		time.year, time.month, time.day, time.hour, time.min, time.sec)
 end
 
@@ -47,6 +50,8 @@ function OnMatchInit()
 	for col = 0, 4 do
 		CreatePlant(34, 2, col)
 	end
+	CreateZombie(32, 2, 8)
+	CreateIZBrain(2)
 end
 
 -- 赛前准备（每帧调用，可用于延迟开赛）
@@ -66,17 +71,17 @@ end
 -- 比赛结束回调
 function OnTerminate(plant_won)
 	-- 输出当前阵型和坚持轮数
-	if result_file then
-		if plant_won then
-			result_file:write("Plant win !")
-		else
-			result_file:write("Zombie win !")
-		end
-
-		result_file:write("\n")
-		result_file:flush()
+	if not plant_won then
+		zombie_win_round = zombie_win_round + 1
 	end
 	
 	-- 递增已完成对局数
 	Match.Round = Match.Round + 1
+	if Match.Round == total then
+		result_file:write(string.format("{\"formation\":\"%s\",\"zombie_wins\":%d,\"trials\":%d}")
+			, "P34|P34|P34|P34|P34|_|_|_|Z32", zombie_win_round, total)
+		result_file:flush()
+
+		exit()
+	end
 end
